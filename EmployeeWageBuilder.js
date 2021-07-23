302e198fb126a6c49b6310c6740afe4b88d3bcaf
Roshni1998 <roshnimali847@gmail.com>
2021-07-24 00:15:07 +0530

    Added : UC-9 Arrow Function

--- EmployeeWageBuilder.js
+++ EmployeeWageBuilder.js
@@ -108,3 +108,27 @@ console.log(empDailyWageMap);
 console.log(empDailyHrsMap);
 console.log("UC 8 - Emp Wage Map totalHrs : " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
 
+// UC-9 Arrow functions
+const findTotal = (totalVal, dailyVal) => {
+	return totalVal + dailyVal;
+}
+let count =0;
+let totalHours = Array.from(empDailyHrsMap.values())
+                      .reduce(findTotal, 0);
+let totalSalary = empDailyWageArr
+                  .filter(dailyWage => dailyWage > 0)
+				  .reduce(findTotal, 0);
+console.log("UC9A - Emp Wage with Arrow. : " + "Total Hours: " + totalHours + ", Total Wages: " + totalSalary);
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value, key, map) => {
+	if (value == 8) fullWorkingDays.push(key);
+	else if (value == 4) partWorkingDays.push(key);
+	else nonWorkingDays.push(key);
+});
+console.log("Full Working Days : " + fullWorkingDays);
+console.log("Part Working Days : " + partWorkingDays);
+console.log("Non Working Days : " + nonWorkingDays);
+
+
