6cd2f9e84ab073a902d39d6bcae19813ea49c81c
Roshni1998 <roshnimali847@gmail.com>
2021-07-23 23:44:04 +0530

    Added : UC-8 Store the day and the daily wage along with the total wage in a Map

--- EmployeeWageBuilder.js
+++ EmployeeWageBuilder.js
@@ -1,5 +1,5 @@
-// UC-2
-// Calculate daily employee wage based on part time or full time work.
+// UC-6 Arrays and 7 Maps
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -31,12 +31,14 @@ let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyHrsAndWageArr = new Array();
 let empDailyWageArr = new Array();
+let empDailyWageMap = new Map();
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 	totalWorkingDays++;
 	let empCheck = Math.floor(Math.random() * 10) % 3;
 	let empHrs = getWorkingHours(empCheck);
 	totalEmpHrs += empHrs;
 	empDailyWageArr.push(calcDailywage(empHrs));
+   empDailyWageMap.set(totalWorkingDays, calcDailywage(empHrs));
 }
 
 let empWage = calcDailywage(totalEmpHrs);
@@ -98,3 +100,8 @@ function totalDaysWorked(numOfDays, dailyWage) {
 	return numOfDays;
 }
 console.log("UC 7G - Number of days emp worked : " + empDailyWageArr.reduce(totalDaysWorked, 0));
+
+// UC-8 Map Function
+console.log(empDailyWageMap);
+console.log("UC 8 - Emp Wage Map totalHrs : " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
